4f6eb0993fee6af3247a9b25c7a1886a0ec74a75
jendiamond <thejendiamond@gmail.com>
2017-07-12 01:55:40 +0000

    WIP Standup

--- app/controllers/route_list_controller.rb
+++ app/controllers/route_list_controller.rb
@@ -4,8 +4,22 @@ require_relative '../../lib/database_mgr'
 class RouteListController < ApplicationController
   @routelist = {}
 
-  def show
-    @metro = MetroRail.new
-    @routelist = @metro.get_route_list
+  def index
+    @routelists = Route.all
+    if params[:route_id]
+      @stoplist = Stop.where(route_id: params[:route_id])
+    end
+  end
+
+
+#  def show
+#    @metro = MetroRail.new
+#    @routelist = @metro.get_route_list
+#  end
+
+  private
+
+  def route_params
+    params.require(:route).permit(:route_id, :display_name, :created_at, :updated_at, :route_location)
   end
 end
