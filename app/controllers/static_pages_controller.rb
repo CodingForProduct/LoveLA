49b5a483aa2200d16c9e13e83dfa3aca0641877d
jendiamond <thejendiamond@gmail.com>
2017-07-13 18:43:24 -0700

    Add a Map of the Metro
    + Add an embedded Google Map of the Metro and the stops so the user can easily
    find their way around and not get lost
    
    modified:   app/assets/stylesheets/application.scss
    deleted:    app/controllers/home_controller.rb
    new file:   app/controllers/static_pages_controller.rb
    modified:   app/views/layouts/_navbar.html.erb
    new file:   app/views/static_pages/gmap.html.erb
    modified:   config/routes.rb

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,4 @@
+class StaticPagesController < ApplicationController
+  def gmap
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
+  get 'static_pages/gmap'
+
   @routelist = {}
   @stoplist = {}
 
   root 'routes#index'
 
-  get '/stops', ':route_id', to: 'stops#index'
+  get '/stops',  ':route_id', to: 'stops#index'
   get '/places', ':longitude',':latitude', to: 'places#index'
-
+  get '/map', to: 'static_pages#gmap'
 end
